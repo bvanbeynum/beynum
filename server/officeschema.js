21cd5f43afc81d8d498f6a3998de659bccc6d4c8
Brett van Beynum <maildrop444@gmail.com>
2021-05-27 17:58:00 +0000

    Convert to tabs

--- server/officeschema.js
+++ server/officeschema.js
@@ -4,20 +4,20 @@ const { model } = mongoose;
 
 export default {
 
-    sensorLog: model("sensorlog", {
-        logTime: Date,
-        temp: Number,
-        humidity: Number,
-        isLight: Boolean,
-        isDoorEvent: Boolean,
-        isDoorOpen: Boolean,
-        hasMotion: Boolean,
-        isFanOn: Boolean
-    }),
+	sensorLog: model("sensorlog", {
+		logTime: Date,
+		temp: Number,
+		humidity: Number,
+		isLight: Boolean,
+		isDoorEvent: Boolean,
+		isDoorOpen: Boolean,
+		hasMotion: Boolean,
+		isFanOn: Boolean
+	}),
 
-    command: model("command", {
-        insertTime: Date,
-        type: String,
-        status: Boolean
-    })
+	command: model("command", {
+		insertTime: Date,
+		type: String,
+		status: Boolean
+	})
 };
