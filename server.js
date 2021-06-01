0f6e59cbbc4d4d4b1ae35a1e653dae7abd809699
Brett van Beynum <maildrop444@gmail.com>
2021-06-01 21:34:56 +0000

    Moved webpack into development

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ import config from "./server/config.js";
 import express from "express";
 import bodyParser from "body-parser";
 import officeRouter from "./server/officerouter.js"
-import webpack from "webpack";
-import webpackDevMiddleware from "webpack-dev-middleware";
 import webpackConfig from "./webpack.dev.js";
 
 // Declarations =======================================================================
@@ -17,7 +15,6 @@ const { json, urlencoded } = bodyParser;
 const port = config.port || 9201;
 const currentFile = fileURLToPath(import.meta.url);
 const currentDirectory = dirname(currentFile);
-const compilier = webpack(webpackConfig);
 
 // Config =======================================================================
 
@@ -31,6 +28,11 @@ app.use(urlencoded({ extended: true }));
 app.use(officeRouter);
 
 if (webpackConfig.mode === "development") {
+	import webpack from "webpack";
+	import webpackDevMiddleware from "webpack-dev-middleware";
+	
+	const compilier = webpack(webpackConfig);
+
 	app.use(webpackDevMiddleware(compilier, { publicPath: webpackConfig.output.publicPath }))
 }
 else {
